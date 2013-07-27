279acfac1c47bd3a4bda41bc204697feed910f8d
Albert Agram <albert.agram@gmail.com>
2013-07-27 19:50:50 +0000

    still sending welcome email

--- app/mailers/myflix_mailer.rb
+++ app/mailers/myflix_mailer.rb
@@ -1,6 +1,6 @@
 class MyflixMailer < ActionMailer::Base
   def send_welcome_email(user)
     @user = user
-    mail from: 'info@myflix.com', to: user.email, subject: "Welcome to MyFlix!"
+    mail from: 'albert.agram@gmail.com', to: user.email, subject: "Welcome to MyFlix!"
   end
 end
