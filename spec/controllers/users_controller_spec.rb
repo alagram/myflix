7ee358f4dee885b85ea62fdca25744392d87583c
Albert Agram <albert.agram@gmail.com>
2013-07-27 20:56:48 +0000

    added tests to email sending

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -41,6 +41,23 @@ describe UsersController do
         expect(assigns(:user)).to be_instance_of(User)
       end
     end
+
+    context "email sending" do
+      it "sends out the email" do
+        post :create, user: Fabricate.attributes_for(:user)
+        expect(ActionMailer::Base.deliveries).to_not be_empty
+      end
+      it "sends to the right receipient" do
+        post :create, user: Fabricate.attributes_for(:user)
+        message = ActionMailer::Base.deliveries.last
+        expect(message.to).to eq([User.first.email])
+      end
+      it "has the right content" do
+        post :create, user: Fabricate.attributes_for(:user)
+        message = ActionMailer::Base.deliveries.last
+        expect(message.body).to include(User.first.email)
+      end
+    end
   end
 
   describe "GET show" do
