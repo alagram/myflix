9c3912230b2e4fd9c34d2674dfe0fc67e5be6f2c
Albert Agram <albert.agram@gmail.com>
2013-08-03 12:20:47 +0000

    refactor with concerns

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  include Tokenable
+
   has_many :reviews, order: "created_at DESC"
   has_many :queue_items, order: :position
   has_many :videos, :through => :queue_items
@@ -11,8 +13,6 @@ class User < ActiveRecord::Base
   validates_uniqueness_of :email
 
   has_secure_password
-
-  before_create :generate_token
 
   def normalize_queue_item_positions
     queue_items.each_with_index do |queue_item, index|
@@ -30,9 +30,5 @@ class User < ActiveRecord::Base
 
   def follow(another_user)
     following_relationships.create(leader: another_user) if can_follow?(another_user)
-  end
-
-  def generate_token
-    self.token = SecureRandom.urlsafe_base64
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,9 +10,8 @@ describe User do
   it { should have_many(:queue_items).order(:position) }
   it { should have_many(:videos).through(:queue_items) }
 
-  it "generates a random token when the user is created" do
-    alice = Fabricate(:user)
-    expect(alice.token).to be_present
+  it_behaves_like "tokenable" do
+    let(:object) { Fabricate(:user) }
   end
 
   describe "#follows?" do
