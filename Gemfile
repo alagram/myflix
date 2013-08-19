850b287db5a4f51d0d93c6c3980aa3e868777477
Albert Agram <albert.agram@gmail.com>
2013-08-19 23:04:24 +0000

    Build API Wrapper for Stripe

--- Gemfile
+++ Gemfile
@@ -39,6 +39,8 @@ group :test do
   gem 'capybara'
   gem 'launchy'
   gem 'capybara-email'
+  gem 'vcr'
+  gem 'webmock', '~> 1.11.0'
 end
 
 group :test, :development do
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,6 +11,12 @@ require 'sidekiq/testing/inline'
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
+VCR.configure do |c|
+  c.cassette_library_dir = 'spec/cassettes'
+  c.hook_into :webmock
+  c.configure_rspec_metadata!
+end
+
 RSpec.configure do |config|
   # ## Mock Framework
   #
@@ -38,4 +44,5 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
+  config.treat_symbols_as_metadata_keys_with_true_values = true
 end
