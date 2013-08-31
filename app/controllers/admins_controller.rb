8a304417fc7028c5931d1b4d81b00aa587ad9915
Albert Agram <albert.agram@gmail.com>
2013-08-31 12:25:30 +0000

    admin view payments feature specs and UI implementation

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,6 +1,8 @@
 class AdminsController < AuthenticatedController
   before_filter :require_admin
 
+  private
+  
   def require_admin
     unless current_user.admin?
       flash[:error] = "You are not authorised to do that."
