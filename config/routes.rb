887b231d467f4a91f84eadc97aaed795c799c6d7
Albert Agram <albert.agram@gmail.com>
2013-09-04 08:31:19 +0000

    begin accounts page

--- config/routes.rb
+++ config/routes.rb
@@ -28,7 +28,7 @@ Myflix::Application.routes.draw do
   end
 
   resources :genres, only: [:show]
-  resources :users, only: [:create, :show]
+  resources :users, only: [:create, :show, :edit]
   resources :sessions, only: [:create]
   resources :queue_items, only: [:create, :destroy]
   resources :relationships, only: [:create, :destroy]
